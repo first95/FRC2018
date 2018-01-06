d91e0e3d7aa2c4ae6353f0352b7a88d5b0688051
John Walthour <john.walthour@gmail.com>
2018-01-06 17:09:51 -0500

    Added chart that graphs throttle vs voltage for a given throttle

--- FRC2018/src/org/usfirst/frc/team95/robot/components/test/AdjustedTalonTester.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/components/test/AdjustedTalonTester.java
@@ -11,6 +11,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -22,9 +23,8 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 	
 	AdjustedTalon uut;
 	TestPowerDistributionPanel pdp = new TestPowerDistributionPanel();
-	private double  lastCommandedLeaderThrottle;
-	private double  lastCommandedLeaderSpeed;
-	private boolean lastCommandedShift;
+	private double  lastCommandedThrottle;
+	private double  lastCommandedSpeed;
 	
 	public AdjustedTalonTester(String title) {
 		super(title);
@@ -36,14 +36,17 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 	public void runThrottleTest() {
 		// Create dataset
 		XYDataset dataset = new XYSeriesCollection();
-		XYSeries series1 = new XYSeries("Adjusted throttle vs input");
 		
-		pdp.setVoltage(13.0);
-		for(double throttle = -1.0; throttle <= 1.0; throttle += 0.05) {
-			uut.set(throttle);
-			series1.add(throttle, lastCommandedLeaderThrottle);
+		for(double voltage = 10; voltage >= 5.0; voltage -= 0.5) {
+			pdp.setVoltage(voltage);
+			XYSeries series = new XYSeries("Adjusted throttle vs input at " + pdp.getVoltage() + "V", false);
+			
+			for(double throttle = -1.0; throttle <= 1.0; throttle += 0.05) {
+				uut.set(throttle);
+				series.add(throttle, lastCommandedThrottle);
+			}
+			((XYSeriesCollection)dataset).addSeries(series);			
 		}
-		((XYSeriesCollection)dataset).addSeries(series1);
 
 		// Create chart
 		JFreeChart chart = ChartFactory.createScatterPlot("Adjusted throttle vs input", "Input throttle", "Adjusted throttle",
@@ -52,6 +55,50 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 		// Changes background color
 		XYPlot plot = (XYPlot) chart.getPlot();
 		plot.setBackgroundPaint(new Color(200, 200, 200));
+		
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, true);
+        renderer.setSeriesShapesVisible(0, false);
+        plot.setRenderer(renderer);
+
+		// Create Panel
+		ChartPanel panel = new ChartPanel(chart);
+		setContentPane(panel);
+	}
+
+	public void runVoltageTest() {
+		// Create dataset
+		XYDataset dataset = new XYSeriesCollection();
+
+		for(double throttle = -1.0; throttle <= 1.0; throttle += 0.2) {
+			// Prime the UUT with normal voltages for a while
+			double voltage = 13;
+			for(int i = 0; i < AdjustedTalon.BACKWARDS_MULTIPLIER; ++i) {
+				pdp.setVoltage(13);
+				uut.set(0.0);
+			}
+			
+			XYSeries series = new XYSeries("Adjusted throttle vs voltage at throttle " + throttle, false);
+			for(; voltage >= 5.0; voltage -= 0.05) {
+				pdp.setVoltage(voltage);
+				uut.set(throttle);
+				series.add(voltage, lastCommandedThrottle);
+			}
+			((XYSeriesCollection)dataset).addSeries(series);			
+		}
+
+		// Create chart
+		JFreeChart chart = ChartFactory.createScatterPlot("Adjusted throttle vs voltage", "voltage", "Adjusted throttle",
+				dataset);
+
+		// Changes background color
+		XYPlot plot = (XYPlot) chart.getPlot();
+		plot.setBackgroundPaint(new Color(200, 200, 200));
+		
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, true);
+        renderer.setSeriesShapesVisible(0, false);
+        plot.setRenderer(renderer);
 
 		// Create Panel
 		ChartPanel panel = new ChartPanel(chart);
@@ -60,12 +107,20 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			AdjustedTalonTester example = new AdjustedTalonTester("Adjusted Talon test");
-			example.runThrottleTest();
-			example.setSize(800, 400);
-			example.setLocationRelativeTo(null);
-			example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-			example.setVisible(true);
+			AdjustedTalonTester example1 = new AdjustedTalonTester("Adjusted Talon test");
+			example1.runThrottleTest();
+			example1.setSize(1000, 1000);
+			example1.setLocationRelativeTo(null);
+			example1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			example1.setVisible(true);
+
+			AdjustedTalonTester example2 = new AdjustedTalonTester("Adjusted Talon test");
+			example2.runVoltageTest();
+			example2.setSize(1000, 1000);
+			example2.setLocationRelativeTo(null);
+			example2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			example2.setVisible(true);
+		
 		});
 	}
 
@@ -73,12 +128,12 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 	// Methods for TestCanSpeedController.Listener
 	@Override
 	public void takeSet(int id, double set) {
-		lastCommandedLeaderThrottle = set;
+		lastCommandedThrottle = set;
 	}
 
 	@Override
 	public void takeSpeed(int id, double speed) {
-		lastCommandedLeaderSpeed = speed;
+		lastCommandedSpeed = speed;
 		
 	}
 
