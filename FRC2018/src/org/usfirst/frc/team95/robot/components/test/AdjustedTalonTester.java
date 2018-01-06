d50cc96dd2ab027d72d0636b3a1443e5d4630c4c
John Walthour <john.walthour@gmail.com>
2018-01-06 17:14:46 -0500

    Cosmetic improvements

--- FRC2018/src/org/usfirst/frc/team95/robot/components/test/AdjustedTalonTester.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/components/test/AdjustedTalonTester.java
@@ -37,15 +37,20 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 		// Create dataset
 		XYDataset dataset = new XYSeriesCollection();
 		
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        int seriesNum = 0;
 		for(double voltage = 10; voltage >= 5.0; voltage -= 0.5) {
 			pdp.setVoltage(voltage);
-			XYSeries series = new XYSeries("Adjusted throttle vs input at " + pdp.getVoltage() + "V", false);
+			XYSeries series = new XYSeries("Battery at " + pdp.getVoltage() + "V", false);
 			
 			for(double throttle = -1.0; throttle <= 1.0; throttle += 0.05) {
 				uut.set(throttle);
 				series.add(throttle, lastCommandedThrottle);
 			}
 			((XYSeriesCollection)dataset).addSeries(series);			
+			renderer.setSeriesLinesVisible(seriesNum, true);
+	        renderer.setSeriesShapesVisible(seriesNum, false);
+	        seriesNum++;
 		}
 
 		// Create chart
@@ -55,11 +60,7 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 		// Changes background color
 		XYPlot plot = (XYPlot) chart.getPlot();
 		plot.setBackgroundPaint(new Color(200, 200, 200));
-		
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesLinesVisible(0, true);
-        renderer.setSeriesShapesVisible(0, false);
-        plot.setRenderer(renderer);
+		plot.setRenderer(renderer);
 
 		// Create Panel
 		ChartPanel panel = new ChartPanel(chart);
@@ -70,7 +71,9 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 		// Create dataset
 		XYDataset dataset = new XYSeriesCollection();
 
-		for(double throttle = -1.0; throttle <= 1.0; throttle += 0.2) {
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        int seriesNum = 0;
+		for(double throttle = -1.0; throttle <= 1.0; throttle += 0.25) {
 			// Prime the UUT with normal voltages for a while
 			double voltage = 13;
 			for(int i = 0; i < AdjustedTalon.BACKWARDS_MULTIPLIER; ++i) {
@@ -78,26 +81,26 @@ public class AdjustedTalonTester extends JFrame implements TestCanSpeedControlle
 				uut.set(0.0);
 			}
 			
-			XYSeries series = new XYSeries("Adjusted throttle vs voltage at throttle " + throttle, false);
+			XYSeries series = new XYSeries("Throttle at " + throttle, false);
 			for(; voltage >= 5.0; voltage -= 0.05) {
 				pdp.setVoltage(voltage);
 				uut.set(throttle);
 				series.add(voltage, lastCommandedThrottle);
 			}
 			((XYSeriesCollection)dataset).addSeries(series);			
+
+			renderer.setSeriesLinesVisible(seriesNum, true);
+	        renderer.setSeriesShapesVisible(seriesNum, false);
+	        seriesNum++;
 		}
 
 		// Create chart
-		JFreeChart chart = ChartFactory.createScatterPlot("Adjusted throttle vs voltage", "voltage", "Adjusted throttle",
+		JFreeChart chart = ChartFactory.createScatterPlot("Adjusted throttle vs battery voltage", "voltage", "Adjusted throttle",
 				dataset);
 
 		// Changes background color
 		XYPlot plot = (XYPlot) chart.getPlot();
 		plot.setBackgroundPaint(new Color(200, 200, 200));
-		
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesLinesVisible(0, true);
-        renderer.setSeriesShapesVisible(0, false);
         plot.setRenderer(renderer);
 
 		// Create Panel
