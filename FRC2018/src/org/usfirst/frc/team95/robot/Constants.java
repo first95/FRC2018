308e4926bb9f6462d45dd6b55389426eeccd4c46
John Walthour <john.walthour@gmail.com>
2018-01-29 19:19:42 -0500

    This is where the shifter solenoid wound up wired

--- FRC2018/src/org/usfirst/frc/team95/robot/Constants.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/Constants.java
@@ -16,7 +16,7 @@ public class Constants
 		public static double FLOOR_INTAKE_THROTTLE = 0.7;
 		public static int CLIMBER_AXIS = 1;
 		
-		public static int SHIFTER_SOLENOID_NUM  = 4;
+		public static int SHIFTER_SOLENOID_NUM  = 0;
 		// Shift button in IO
 		public static int SHIFT_BUTTON = 2; // TODO: Pick a good button
 		
