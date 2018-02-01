ca231c3f85599d1f5acdc2631c0d0d03ef478260
RoboticsNA <nicholas.arnold@mvrsd.org>
2018-01-31 21:34:28 -0500

    atonomousCommand.cancel(); causes errors, also elevator and collector/wrist works! James wants a deadzone on the elevator controls and more versions of the wrist need to be added, specifcally a both pistons deactivated mode also the wrist has to be a toggle.

--- FRC2018/src/org/usfirst/frc/team95/robot/Robot.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/Robot.java
@@ -128,7 +128,7 @@ public class Robot extends IterativeRobot
 				// teleop starts running. If you want the autonomous to
 				// continue until interrupted by another command, remove
 				// this line or comment it out.
-				autonomousCommand.cancel();
+				// autonomousCommand.cancel();
 			}
 
 		/**
