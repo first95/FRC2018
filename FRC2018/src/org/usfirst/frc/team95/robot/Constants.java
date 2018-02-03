d60999a7ca0383020fbd5efef29ee9bbc6b948d0
John Walthour <john.walthour@gmail.com>
2018-02-02 20:04:46 -0500

    Updated constants to be finals, which protects them from modification

--- FRC2018/src/org/usfirst/frc/team95/robot/Constants.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/Constants.java
@@ -7,36 +7,35 @@ public class Constants
 		public static double joystickDeadbandH = 0.05;
 
 		// Used with Talons
-		public static int PID_IDX = 0; // The Talons support up to 2 PID loops, with indexes 0 and 1.  We only use 0.
-		public static int CAN_TIMEOUT_MS = 10; // The amount of time to wait for the CAN transaction to finish
+		public static final int PID_IDX = 0; // The Talons support up to 2 PID loops, with indexes 0 and 1.  We only use 0.
+		public static final int CAN_TIMEOUT_MS = 10; // The amount of time to wait for the CAN transaction to finish
 		
 		// Indices for solenoids
-		public static int SHIFTER_SOLENOID_NUM  = 0;
-		public static int COLLECTOR_SOLENOID_NUM  = 1;
-		public static int WRIST_STAGE_ONE = 2;
-		public static int WRIST_STAGE_TWO = 3;
+		public static final int SHIFTER_SOLENOID_NUM  = 0;
+		public static final int COLLECTOR_SOLENOID_NUM  = 1;
+		public static final int WRIST_STAGE_ONE = 2;
+		public static final int WRIST_STAGE_TWO = 3;
 
 		// Indices for Talons
 		// Drive base
-		public static int LEFT_LEAD = 10;
-		public static int LEFT_F1 = 11;
-		public static int LEFT_F2 = 12;
-		public static int RIGHT_LEAD = 20;
-		public static int RIGHT_F1 = 21;
-		public static int RIGHT_F2 = 22;
+		public static final int LEFT_LEAD = 10;
+		public static final int LEFT_F1 = 11;
+		public static final int LEFT_F2 = 12;
+		public static final int RIGHT_LEAD = 20;
+		public static final int RIGHT_F1 = 21;
+		public static final int RIGHT_F2 = 22;
 		
 		// Elevator
-		public static int LEFT_ELEV_DRIVER = 13;
-		public static int RIGHT_ELEV_DRIVER = 23;
+		public static final int LEFT_ELEV_DRIVER = 13;
+		public static final int RIGHT_ELEV_DRIVER = 23;
 
 		// Collector
-		public static int LEFT_CHAIN_DRIVER  = 14;
-		public static int RIGHT_CHAIN_DRIVER = 24;
+		public static final int LEFT_CHAIN_DRIVER  = 14;
+		public static final int RIGHT_CHAIN_DRIVER = 24;
 		
-		// Constants
 		// Encoder values recycled from last year
-		public final static double ENCODER_TICKS_PER_FOOT = 1002;
-		public final static double ENCODER_TICKS_PER_RADIAN = 1637.39265;//1100
+		public static final double ENCODER_TICKS_PER_FOOT = 1002;
+		public static final double ENCODER_TICKS_PER_RADIAN = 1637.39265;//1100
 
 		public static double RFVoltsToFt(double voltage)
 			{
