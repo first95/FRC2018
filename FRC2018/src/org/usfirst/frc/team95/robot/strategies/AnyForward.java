d89fecd71bb0aee54260bb23b948ce40579b722e
John Walthour <john.walthour@gmail.com>
2018-02-13 22:43:58 -0500

    Oh wait - we don't want to drive forward if we're in the center position, because there's a pile of cubes there.

--- FRC2018/src/org/usfirst/frc/team95/robot/strategies/AnyForward.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/strategies/AnyForward.java
@@ -6,20 +6,24 @@ import org.usfirst.frc.team95.robot.commands.drivebase.DriveFromWallToAutoLine;
 
 public class AnyForward extends Strategy {
 	
-	// This strategy does nothing.
+	// This strategy drives past the auto line, unless we're in the center.
 
 	
 	public static final String DESCRIPTION = "Drive forward past auto line";
 	
 	public AnyForward() {
-		addSequential(new DriveFromWallToAutoLine());
+		
 	}
 
 	@Override
 	public void AdjustStrategy(FieldSide whichSideOfTheNearSwitchIsOurColor,
 			FieldSide whichSideOfTheScaleIsOurColor,
 			Robot.StartPosition robotStartingPosition) {
-		// This move is the same in all configurations
+		if(robotStartingPosition != Robot.StartPosition.CENTER) {
+			addSequential(new DriveFromWallToAutoLine());
+		} else {
+			// Don't want to crash into the cubes
+		}
 	}
 
 }
