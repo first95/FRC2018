f090cbf564dba7eb3c3aeeab8a1ff6225287b4d0
John Walthour <john.walthour@gmail.com>
2018-02-13 22:51:16 -0500

    UPdated distance

--- FRC2018/src/org/usfirst/frc/team95/robot/commands/drivebase/DriveFromWallToAutoLine.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/commands/drivebase/DriveFromWallToAutoLine.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team95.robot.commands.drivebase;
 
 public class DriveFromWallToAutoLine extends DriveStraight {
-	public static final double INCHES_TO_AUTO_LINE = 5*12;  // TODO: Measure
+	public static final double INCHES_TO_AUTO_LINE = 10*12;
 
 	public DriveFromWallToAutoLine() {
 		super(INCHES_TO_AUTO_LINE);
