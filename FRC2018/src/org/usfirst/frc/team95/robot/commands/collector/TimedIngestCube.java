512491b9387ead329592a4482c103ff9e752a198
John Walthour <john.walthour@gmail.com>
2018-02-17 17:01:24 -0500

    This one works!  Had some things backward

--- FRC2018/src/org/usfirst/frc/team95/robot/commands/collector/TimedIngestCube.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/commands/collector/TimedIngestCube.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team95.robot.commands.collector;
 
 public class TimedIngestCube extends RunChainsForDuration {
-	public static final double INGEST_THROTTLE = 1.0;
+	public static final double INGEST_THROTTLE = -1.0;
 	public static final double INGEST_TIME_S = 0.25;
 	
 	public TimedIngestCube() {
