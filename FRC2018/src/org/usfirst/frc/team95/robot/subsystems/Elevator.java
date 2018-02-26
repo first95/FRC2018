bc2cda29be99344d3eea3b27df064e15f05db408
John Walthour <john.walthour@gmail.com>
2018-02-26 18:40:08 -0500

    Nope, don't want to do that.  This is handled by the soft limit switch logic.

--- FRC2018/src/org/usfirst/frc/team95/robot/subsystems/Elevator.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/subsystems/Elevator.java
@@ -99,7 +99,6 @@ public class Elevator extends Subsystem {
 	 */
 	public void setCurrentPosToZero() {
 		rightElevDriver.setSelectedSensorPosition(0, Constants.PID_IDX, Constants.CAN_TIMEOUT_MS);
-		rightElevDriver.set(ControlMode.PercentOutput, 0);
 	}
 
 	@Override
