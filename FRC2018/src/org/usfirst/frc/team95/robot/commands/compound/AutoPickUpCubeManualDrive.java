57e83193127a638fdb18ba517134737c6319df1c
RoboticsNA <nicholas.arnold@mvrsd.org>
2018-03-03 07:36:13 -0500

    Auto cube grab fix

--- FRC2018/src/org/usfirst/frc/team95/robot/commands/compound/AutoPickUpCubeManualDrive.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/commands/compound/AutoPickUpCubeManualDrive.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team95.robot.commands.compound;
 
+import org.usfirst.frc.team95.robot.Robot;
 import org.usfirst.frc.team95.robot.commands.Nothing;
 import org.usfirst.frc.team95.robot.commands.collector.AutoCloseMawOnCube;
 import org.usfirst.frc.team95.robot.commands.collector.RunChains;
@@ -18,8 +19,15 @@ public class AutoPickUpCubeManualDrive extends CommandGroup {
 		addSequential(new RunChains(AutoCloseMawOnCube.INTAKE_THROTTLE)); // Start the chains running before we start moving the pneumatics
 		addSequential(new SetWristAngle(WristAngle.DOWN));
 		addSequential(new AutoCloseMawOnCube()); // This one waits until the cube is detected
-		addSequential(new SetWristAngle(WristAngle.MID_UP));
 		addSequential(new TimedIngestCube()); // This will stop the chains once it's done
+		addSequential(new SetWristAngle(WristAngle.MID_UP));
 		addSequential(new Nothing());// Without this, holding the button will restart the move after it completes
 	}
+	
+	@Override
+	protected void end() {
+		addSequential(new SetWristAngle(WristAngle.MID_UP));
+		super.end();
+	}
+	
 }
