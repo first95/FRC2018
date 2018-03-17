f68973b42f843445d28598cc143450b2052f2b08
RoboticsNA <nicholas.arnold@mvrsd.org>
2018-03-17 12:42:30 -0400

    Removed 18inchs from the center intial move so we don't smash the cubes

--- FRC2018/src/org/usfirst/frc/team95/robot/commands/compound/ScoreStartingCubeOnSwitch.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/commands/compound/ScoreStartingCubeOnSwitch.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team95.robot.commands.compound;
 import org.usfirst.frc.team95.robot.commands.collector.EjectCube;
 import org.usfirst.frc.team95.robot.commands.collector.SetWristAngle;
 import org.usfirst.frc.team95.robot.commands.collector.SetWristAngle.WristAngle;
+import org.usfirst.frc.team95.robot.commands.drivebase.DriveStraight;
 import org.usfirst.frc.team95.robot.commands.elevator.SetElevatorHeight;
 import org.usfirst.frc.team95.robot.commands.elevator.SetElevatorHeight.ElevatorHoldPoint;
 
