3c0e9737d936a98e88a7d8528ceb009d5abb179e
Shared02 <lindsayvallen@gmail.com>
2018-03-23 10:41:15 -0400

    2nd Layer PID added

--- FRC2018/src/org/usfirst/frc/team95/robot/commands/drivebase/Pivot.java
+++ FRC2018/src/org/usfirst/frc/team95/robot/commands/drivebase/Pivot.java
@@ -4,7 +4,13 @@
  */
 package org.usfirst.frc.team95.robot.commands.drivebase;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team95.robot.Robot;
 
@@ -17,10 +23,52 @@ import org.usfirst.frc.team95.robot.Robot;
 public class Pivot extends Command {
 	double degreesCw;
 	double robotHeadingAtStartOfMove;
-	private final double TURN_DEGREE_CORRECTION = 18.0;
+	//private final double TURN_DEGREE_CORRECTION = 18.0;
+	
+	private static final double K_P = 35;
+	private static final double K_I = 0.0;
+	private static final double K_D = 0.0;
+	private static final double PIVOT_SPEED = 24;
+	private static final double END_STOP_TOLERANCE = 5;
+	
+	private PIDController clp;
 	
 	public Pivot(double degreesCw) {
 		requires(Robot.drivebase);
+		
+		clp = new PIDController(K_P, K_I, K_D, new PIDSource() {
+			
+			@Override
+			public void setPIDSourceType(PIDSourceType pidSource) {
+			
+			}
+			
+			@Override
+			public double pidGet() {
+				return Robot.drivebase.getRobotHeadingDegrees();
+			}
+			
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return PIDSourceType.kDisplacement;
+			}
+		}, new PIDOutput() {
+			
+			@Override
+			public void pidWrite(double output) {
+				Robot.drivebase.setPivotRate(output);
+			}
+		});
+		
+		clp.setInputRange(0, 360);
+		clp.setOutputRange(0, PIVOT_SPEED);
+		clp.setContinuous(true);
+		clp.setPercentTolerance(END_STOP_TOLERANCE);
+		
+		SmartDashboard.putData("PIDController", clp);
+		SmartDashboard.putNumber("PIDController F Value", clp.getF());
+		SmartDashboard.putNumber("target pos", clp.getSetpoint();
+		SmartDashboard.putNumber("Error", clp.getError());
 		
 		this.degreesCw = degreesCw;
 	}
@@ -30,36 +78,41 @@ public class Pivot extends Command {
 	public void initialize() {
 		System.out.println("Starting Pivot (" + degreesCw + " degrees)");
 		robotHeadingAtStartOfMove = Robot.drivebase.getRobotHeadingDegrees();
-
+		
 		// Command the movement
+		clp.enable();
+		clp.setSetpoint((robotHeadingAtStartOfMove + degreesCw) % 360);
+		
 		//Robot.drivebase.pivotDegreesClockwise(degreesCw);
-		Robot.drivebase.setPivotRate(degreesCw > 0 ? 36 : -36);
+		//Robot.drivebase.setPivotRate(degreesCw > 0 ? 36 : -36);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
+		return clp.onTarget();
 		// Heading is measured with positive headings being counter-clockwise
-		if(degreesCw > 0) {
-			double headingDegrees = Robot.drivebase.getRobotHeadingDegrees();
-			double lessThanValueToCheck = robotHeadingAtStartOfMove - (degreesCw - TURN_DEGREE_CORRECTION);
-			//System.out.println("Heading Degree " + headingDegrees);
-			//System.out.println("Less Than Value " + lessThanValueToCheck);
-			return (headingDegrees < (lessThanValueToCheck));
-		} else if (degreesCw < 0) {
-			double headingDegrees = Robot.drivebase.getRobotHeadingDegrees();
-			double greaterThanValueToCheck = robotHeadingAtStartOfMove - (degreesCw + TURN_DEGREE_CORRECTION);
-			//System.out.println("Heading Degree " + headingDegrees);
-			//System.out.println("Greater Than Value " + greaterThanValueToCheck);
-			return (headingDegrees > (greaterThanValueToCheck));
-		} else { // degreesCW == 0
-			return true;
-		}
+//		if(degreesCw > 0) {
+//			double headingDegrees = Robot.drivebase.getRobotHeadingDegrees();
+//			double lessThanValueToCheck = robotHeadingAtStartOfMove - (degreesCw - TURN_DEGREE_CORRECTION);
+//			//System.out.println("Heading Degree " + headingDegrees);
+//			//System.out.println("Less Than Value " + lessThanValueToCheck);
+//			return (headingDegrees < (lessThanValueToCheck));
+//		} else if (degreesCw < 0) {
+//			double headingDegrees = Robot.drivebase.getRobotHeadingDegrees();
+//			double greaterThanValueToCheck = robotHeadingAtStartOfMove - (degreesCw + TURN_DEGREE_CORRECTION);
+//			//System.out.println("Heading Degree " + headingDegrees);
+//			//System.out.println("Greater Than Value " + greaterThanValueToCheck);
+//			return (headingDegrees > (greaterThanValueToCheck));
+//		} else { // degreesCW == 0
+//			return true;
+//		}
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
+		clp.disable();
 		System.out.println("Ending Pivot (" + degreesCw + " degrees)");
 		Robot.drivebase.drive(0, 0);
 	}
